d881d49e156da557eaf5a6ec48eae5984a9f6bc4
Martin Felix Joergensen <mj@mjoergensen.dk>
2023-03-11 22:21:20 +0100

    Clang-format

--- C++_simple/concurrent_programming/cpp11_atomics.cpp
+++ C++_simple/concurrent_programming/cpp11_atomics.cpp
@@ -1,29 +1,28 @@
-// g++ -std=c++20 cpp11_atomics.cpp && ./a.out                 
-//counter value is: 3%                                                                                                                       
+// g++ -std=c++20 cpp11_atomics.cpp && ./a.out
+// counter value is: 3%
 
-// From: https://medium.com/@joao_vaz/spin-lock-in-modern-c-with-atomics-memory-barriers-and-exponential-back-off-522798aca817
+// From:
+// https://medium.com/@joao_vaz/spin-lock-in-modern-c-with-atomics-memory-barriers-and-exponential-back-off-522798aca817
 
 #include <atomic>
-#include <thread>
 #include <iostream>
+#include <thread>
 
 std::atomic<int> counter{0};
 
-void increment_counter() {
-    counter++;
-}
+void increment_counter() { counter++; }
 
 int main() {
-    std::thread t1(increment_counter);
-    std::thread t2(increment_counter);
-    std::thread t3(increment_counter);
+  std::thread t1(increment_counter);
+  std::thread t2(increment_counter);
+  std::thread t3(increment_counter);
 
-    t1.join();
-    t2.join();
-    t3.join();
+  t1.join();
+  t2.join();
+  t3.join();
 
-    // counter should be equal to 3
-    std::cout << "counter value is: " <<  counter.load();
+  // counter should be equal to 3
+  std::cout << "counter value is: " << counter.load();
 }
 
 /*
@@ -80,4 +79,3 @@ provided to make it easy to use atomics. Here's an example of how to use an
 atomic int variable:
 
 */
-
