37634b0eb443d626f918447fbd8252bad05d95b4
Martin Felix Joergensen <mj@mjoergensen.dk>
2023-03-11 22:21:20 +0100

    Clang-format

--- C++_simple/concurrent_programming/cpp11_atomics_with_memory_barrier.cpp
+++ C++_simple/concurrent_programming/cpp11_atomics_with_memory_barrier.cpp
@@ -1,11 +1,12 @@
 // g++ -std=c++20 cpp11_atomics_with_memory_barrier.cpp && ./a.out
-//Value we were expecting is correct: 5
+// Value we were expecting is correct: 5
 
-// From: https://medium.com/@joao_vaz/spin-lock-in-modern-c-with-atomics-memory-barriers-and-exponential-back-off-522798aca817
+// From:
+// https://medium.com/@joao_vaz/spin-lock-in-modern-c-with-atomics-memory-barriers-and-exponential-back-off-522798aca817
 
 #include <atomic>
-#include <thread>
 #include <iostream>
+#include <thread>
 
 /* In this example, we use the fetch_add() method with
  * std::memory_order_relaxed to increment the counter variable. The
@@ -16,32 +17,30 @@
 
 std::atomic<int> counter(0);
 
-void increment_counter() {
-    counter.fetch_add(1, std::memory_order_relaxed);
-}
+void increment_counter() { counter.fetch_add(1, std::memory_order_relaxed); }
 
 int main() {
-    std::thread t1(increment_counter);
-    std::thread t2(increment_counter);
-    std::thread t3(increment_counter);
-    std::thread t4(increment_counter);
-    std::thread t5(increment_counter);
+  std::thread t1(increment_counter);
+  std::thread t2(increment_counter);
+  std::thread t3(increment_counter);
+  std::thread t4(increment_counter);
+  std::thread t5(increment_counter);
 
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
-    t5.join();
+  t1.join();
+  t2.join();
+  t3.join();
+  t4.join();
+  t5.join();
 
+  // counter should be equal to 5
+  int expected = 5;
+  int got = counter.load(std::memory_order_relaxed);
+  if (got != expected) {
+    std::cout << "Value not correct! We were expecting: " << expected
+              << " but got: " << got << '\n';
+  }
 
-    // counter should be equal to 5
-    int expected = 5;
-    int got = counter.load(std::memory_order_relaxed);
-    if (got != expected) {
-        std::cout << "Value not correct! We were expecting: " << expected << " but got: " << got << '\n';
-    }
-
-    std::cout << "Value we were expecting is correct: " << got << '\n';
+  std::cout << "Value we were expecting is correct: " << got << '\n';
 }
 
 /*
